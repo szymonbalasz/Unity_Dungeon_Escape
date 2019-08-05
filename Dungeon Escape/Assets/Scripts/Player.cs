8e42389e79159c61bcd08ab23e248a04aa86a10b
Szymon Balasz <sbalasz@gmail.com>
2019-08-05 22:09:35 +0200

    added assets, demo scene, player movement

---
+++ Dungeon Escape/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D myBody;
+
+    [SerializeField] float moveSpeed = 1f;
+    [SerializeField] float jumpForce = 5f;
+    private bool grounded;
+    
+    void Start()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
+        myBody.velocity = new Vector2(horizontalInput, myBody.velocity.y);
+    }
+}
