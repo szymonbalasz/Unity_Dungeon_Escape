d6da2369a094a3ff46530861b18635c5bc0dd600
Szymon Balasz <sbalasz@gmail.com>
2019-08-06 21:51:47 +0200

    moved imported assets to root assets folder. added jumping mechanic. added run animation

--- Dungeon Escape/Assets/Scripts/Player.cs
+++ Dungeon Escape/Assets/Scripts/Player.cs
@@ -6,9 +6,10 @@ public class Player : MonoBehaviour
 {
     private Rigidbody2D myBody;
 
-    [SerializeField] float moveSpeed = 1f;
-    [SerializeField] float jumpForce = 5f;
-    private bool grounded;
+    [SerializeField] private float moveSpeed = 2f;
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private LayerMask groundLayer;
+    private bool resetJump = false;
     
     void Start()
     {
@@ -18,11 +19,42 @@ public class Player : MonoBehaviour
     void Update()
     {
         Move();
+        Jump();
     }
 
     private void Move()
     {
-        float horizontalInput = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
+        float horizontalInput = Input.GetAxisRaw("Horizontal") * Time.deltaTime * moveSpeed;
         myBody.velocity = new Vector2(horizontalInput, myBody.velocity.y);
     }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded())
+        {
+            myBody.velocity = new Vector2(myBody.velocity.x, jumpForce);
+            StartCoroutine(ResetJumpRoutine());
+        }
+    }
+
+    private bool isGrounded()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 0.6f, groundLayer.value);
+        if(hitInfo.collider != null)
+        {
+            if (resetJump == false)
+            {
+                return true;
+            }            
+        }
+        return false;
+    }
+
+    IEnumerator ResetJumpRoutine()
+    {
+        resetJump = true;
+        yield return new WaitForSeconds(0.1f);
+        resetJump = false;
+    }
+
 }
