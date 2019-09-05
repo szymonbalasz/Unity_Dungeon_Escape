2d8cb102a781b4800b0da18a6e4bae596b6bb21e
Szymon Balasz <sbalasz@gmail.com>
2019-09-05 08:21:18 +0200

    Update Spider.cs

--- Dungeon Escape/Assets/Scripts/Enemy/Spider.cs
+++ Dungeon Escape/Assets/Scripts/Enemy/Spider.cs
@@ -4,27 +4,6 @@ using UnityEngine;
 
 public class Spider : Enemy, IDamageable
 {
-    public int Health { get; set; }
-
-    public override void Init()
-    {
-        base.Init();
-        Health = base.health;
-    }
-
-    public void Damage()
-    {
-        Health--;
-        if (Health < 1)
-        {
-            StartCoroutine(Death());
-        }
-        else
-        {
-            Hit();
-        }
-    }
-
     public override void Hit()
     {
         return;
