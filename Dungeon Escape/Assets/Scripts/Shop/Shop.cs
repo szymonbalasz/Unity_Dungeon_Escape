179b0b024767d79f27ee36e75d6027b704d74b83
Szymon Balasz <sbalasz@gmail.com>
2019-09-08 21:08:15 +0200

    implemented shop

---
+++ Dungeon Escape/Assets/Scripts/Shop/Shop.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    [SerializeField] GameObject shopPanel;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            shopPanel.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            shopPanel.SetActive(false);
+        }
+    }
+}
